674edf3a93432561199eeb4edfb106056c93a571
yuxing <dingguangren@126.com>
2022-07-11 22:54:17 +0800

    学习openpyxl 从excel中读取图片的方法

--- learn_openpyxl.py
+++ learn_openpyxl.py
@@ -1,4 +1,7 @@
+import io
+
 import openpyxl
+from openpyxl_image_loader import SheetImageLoader
 
 workbook = openpyxl.load_workbook('test.xlsx')
 
@@ -21,7 +24,15 @@ y1 = sheet['Y1']
 print(y1.value)
 print(type(y1.value))
 
+# 从excel 中读取图片保存值本地
+# 安装依赖：pip install openpyxl-image-loader
+image_loader = SheetImageLoader(sheet)
+# 获取图片
+# C2 表示图片所在单元格，需要注意：图片不能嵌入单元格中，否则无法读取
+image = image_loader.get('C2')
+# 保存文件到本地
+image.save('temp/1.png')
 
-
-
-
+# 获取字节流
+# buf = io.BytesIO()
+# image.save(buf, format='PNG')
